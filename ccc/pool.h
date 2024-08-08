d008433a4caf3fcbf8fa97b7397fbed4218d5860
Alexander Lopez <agl.alexglopez@gmail.com>
2024-08-07 20:33:44 -0700

    a pool will not work if pointers need to remain stable for a container

--- ccc/pool.h
+++ ccc/pool.h
@@ -14,7 +14,7 @@ typedef enum ccc_pool_result
     CCC_POOL_ERR,
 } ccc_pool_result;
 
-typedef void *ccc_pool_realloc_fn(void *, size_t);
+typedef void *ccc_pool_malloc_fn(size_t);
 
 typedef struct ccc_pool
 {
@@ -22,19 +22,16 @@ typedef struct ccc_pool
     size_t elem_sz ATTRIB_PRIVATE;
     size_t sz ATTRIB_PRIVATE;
     size_t capacity ATTRIB_PRIVATE;
-    ccc_pool_realloc_fn *fn ATTRIB_PRIVATE;
 } ccc_pool;
 
 ccc_pool_result ccc_pool_init(ccc_pool[static 1], size_t elem_sz,
-                              size_t capacity, ccc_pool_realloc_fn *fn)
+                              size_t capacity, ccc_pool_malloc_fn *fn)
     ATTRIB_NONNULL(1);
 void *ccc_pool_base(ccc_pool[static 1]) ATTRIB_NONNULL(1);
 size_t ccc_pool_size(ccc_pool const[static 1]) ATTRIB_NONNULL(1);
 size_t ccc_pool_capacity(ccc_pool const[static 1]) ATTRIB_NONNULL(1);
 bool ccc_pool_full(ccc_pool const[static 1]) ATTRIB_NONNULL(1);
 bool ccc_pool_empty(ccc_pool const[static 1]) ATTRIB_NONNULL(1);
-ccc_pool_result ccc_pool_realloc(ccc_pool[static 1], size_t new_capacity)
-    ATTRIB_NONNULL(1);
 void *ccc_pool_at(ccc_pool const[static 1], size_t) ATTRIB_NONNULL(1);
 void *ccc_pool_alloc(ccc_pool[static 1]) ATTRIB_NONNULL(1);
 ccc_pool_result ccc_pool_pop(ccc_pool[static 1]) ATTRIB_NONNULL(1);
