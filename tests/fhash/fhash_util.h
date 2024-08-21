a070b69bb335dab12ceed79d5317da855eae752c
Alexander Lopez <agl.alexglopez@gmail.com>
2024-08-20 21:51:47 -0700

    use impl struct idiom regardless of single field

--- tests/fhash/fhash_util.h
+++ tests/fhash/fhash_util.h
@@ -12,8 +12,8 @@ struct val
     ccc_fhash_elem e;
 };
 
-int64_t fhash_val_zero(void const *);
-int64_t fhash_val_last_digit(void const *);
+uint64_t fhash_val_zero(void const *);
+uint64_t fhash_val_last_digit(void const *);
 bool fhash_val_eq(void const *, void const *, void *aux);
 bool fhash_id_and_val_eq(void const *, void const *, void *aux);
 
