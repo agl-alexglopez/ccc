6fef84310f4339981dbbe005a4c67e75a07fcb8e
Alexander Lopez <agl.alexglopez@gmail.com>
2024-08-23 09:06:46 -0700

    put args in parens

--- ccc/macros.h
+++ ccc/macros.h
@@ -9,27 +9,28 @@
         _or_insert_res_;                                                       \
     })
 
-#define ENTRY(ptr, key)                                                        \
-    _Generic((ptr),                                                            \
-        ccc_fhash *: (ccc_fhash_entry){CCC_IMPL_FH_ENTRY(ptr, key)},           \
+#define ENTRY(container_ptr, key)                                              \
+    _Generic((container_ptr),                                                  \
+        ccc_fhash                                                              \
+            *: (ccc_fhash_entry){CCC_IMPL_FH_ENTRY((container_ptr), (key))},   \
         default: DEFAULT_ERR)
 
 #define OR_INSERT_WITH(entry_copy, struct_key_value_initializer...)            \
     _Generic((entry_copy),                                                     \
         ccc_fhash_entry: CCC_IMPL_FH_OR_INSERT_WITH(                           \
-                 entry_copy, struct_key_value_initializer),                    \
+                 (entry_copy), (struct_key_value_initializer)),                \
         default: DEFAULT_ERR)
 
 #define AND_MODIFY(entry_copy, mod_fn)                                         \
     _Generic((entry_copy),                                                     \
-        ccc_fhash_entry: (                                                     \
-                 ccc_fhash_entry){CCC_IMPL_FH_AND_MODIFY(entry_copy, mod_fn)}, \
+        ccc_fhash_entry: (ccc_fhash_entry){CCC_IMPL_FH_AND_MODIFY(             \
+            (entry_copy), (mod_fn))},                                          \
         default: DEFAULT_ERR)
 
 #define AND_MODIFY_WITH(entry_copy, mod_fn, aux)                               \
     _Generic((entry_copy),                                                     \
         ccc_fhash_entry: (ccc_fhash_entry){CCC_IMPL_FH_AND_MODIFY_WITH(        \
-            entry_copy, mod_fn, aux)},                                         \
+            (entry_copy), (mod_fn), (aux))},                                   \
         default: DEFAULT_ERR)
 
 #endif /* CCC_MACROS_H */
