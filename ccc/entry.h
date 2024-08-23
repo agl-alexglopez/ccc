3116272c364409290d8c3ac9c81a5e5b3f0941e3
Alexander Lopez <agl.alexglopez@gmail.com>
2024-08-23 15:42:22 -0700

    _Generic provides compile time error checking no default needed

--- ccc/entry.h
+++ ccc/entry.h
@@ -3,34 +3,24 @@
 
 #include "impl_flat_hash.h"
 
-#define CCC_DEFAULT_ERR                                                        \
-    ({                                                                         \
-        ccc_result _input_err_res_ = CCC_INPUT_ERR;                            \
-        _input_err_res_;                                                       \
-    })
-
 #define ENTRY(container_ptr, key)                                              \
     _Generic((container_ptr),                                                  \
         ccc_fhash                                                              \
-            *: (ccc_fhash_entry){CCC_IMPL_FH_ENTRY((container_ptr), (key))},   \
-        default: CCC_DEFAULT_ERR)
+            *: (ccc_fhash_entry){CCC_IMPL_FH_ENTRY((container_ptr), (key))})
 
 #define OR_INSERT_WITH(entry_copy, struct_key_value_initializer...)            \
     _Generic((entry_copy),                                                     \
         ccc_fhash_entry: CCC_IMPL_FH_OR_INSERT_WITH(                           \
-                 (entry_copy), (struct_key_value_initializer)),                \
-        default: CCC_DEFAULT_ERR)
+                 (entry_copy), (struct_key_value_initializer)))
 
 #define AND_MODIFY(entry_copy, mod_fn)                                         \
     _Generic((entry_copy),                                                     \
-        ccc_fhash_entry: (ccc_fhash_entry){CCC_IMPL_FH_AND_MODIFY(             \
-            (entry_copy), (mod_fn))},                                          \
-        default: CCC_DEFAULT_ERR)
+        ccc_fhash_entry: (ccc_fhash_entry){                                    \
+            CCC_IMPL_FH_AND_MODIFY((entry_copy), (mod_fn))})
 
 #define AND_MODIFY_WITH(entry_copy, mod_fn, aux)                               \
     _Generic((entry_copy),                                                     \
-        ccc_fhash_entry: (ccc_fhash_entry){CCC_IMPL_FH_AND_MODIFY_WITH(        \
-            (entry_copy), (mod_fn), (aux))},                                   \
-        default: CCC_DEFAULT_ERR)
+        ccc_fhash_entry: (ccc_fhash_entry){                                    \
+            CCC_IMPL_FH_AND_MODIFY_WITH((entry_copy), (mod_fn), (aux))})
 
 #endif /* CCC_ENTRY_H */
