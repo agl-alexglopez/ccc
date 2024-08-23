86bda4b85d6376dd7634accbe09c032283dce03a
Alexander Lopez <agl.alexglopez@gmail.com>
2024-08-22 19:31:16 -0700

    possible insert only candidate

--- ccc/flat_hash.h
+++ ccc/flat_hash.h
@@ -46,15 +46,21 @@ bool ccc_fh_empty(ccc_fhash const *);
 size_t ccc_fh_size(ccc_fhash const *);
 
 bool ccc_fh_contains(ccc_fhash *, void const *key);
+
+ccc_fhash_entry ccc_fh_insert(ccc_fhash *, void const *key,
+                              ccc_fhash_elem *elem);
+
 ccc_fhash_entry ccc_fh_entry(ccc_fhash *, void const *key);
 ccc_fhash_entry ccc_fh_and_modify(ccc_fhash_entry, ccc_update_fn *);
 ccc_fhash_entry ccc_fh_and_modify_with(ccc_fhash_entry, ccc_update_fn *,
                                        void *aux);
-void *ccc_fh_or_insert(ccc_fhash_entry, ccc_fhash_elem *);
-void *ccc_fh_and_erase(ccc_fhash_entry, ccc_fhash_elem *);
+
+void *ccc_fh_or_insert(ccc_fhash_entry, ccc_fhash_elem *elem);
+void *ccc_fh_and_erase(ccc_fhash_entry, ccc_fhash_elem *elem);
 void const *ccc_fh_get(ccc_fhash_entry);
 void *ccc_fh_get_mut(ccc_fhash_entry);
 bool ccc_fh_occupied(ccc_fhash_entry);
+bool ccc_fh_insert_error(ccc_fhash_entry);
 
 void const *ccc_fh_begin(ccc_fhash const *);
 void const *ccc_fh_next(ccc_fhash const *, ccc_fhash_elem const *iter);
