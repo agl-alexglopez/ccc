80711f72554d5a9887c92b6d4d2ad4f0bad4ecae
Alex Lopez <agl.alexglopez@gmail.com>
2024-09-14 21:11:59 -0700

    entry insert entry to open maze

--- samples/maze.c
+++ samples/maze.c
@@ -216,7 +216,7 @@ animate_maze(struct maze *maze)
         .p = pick_rand_point(maze),
         .cost = rand_range(0, 100),
     };
-    (void)ccc_om_insert(&cell_costs, &odd_point->elem, &(struct point_cost){});
+    (void)insert_entry(entry_vr(&cell_costs, &odd_point->p), &odd_point->elem);
     struct priority_cell *start = valid_malloc(sizeof(struct priority_cell));
     *start = (struct priority_cell){
         .cell = odd_point->p,
