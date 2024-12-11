62d0ef1384c754d67a8a1918d39bcb8aaf6c418d
Alexander Lopez <agl.alexglopez@gmail.com>
2024-12-11 09:15:37 -0800

    add validation checks

--- tests/fomap/test_fomap_insert.c
+++ tests/fomap/test_fomap_insert.c
@@ -107,8 +107,10 @@ CHECK_BEGIN_STATIC_FN(fomap_test_insert_and_find)
     {
         ccc_entry e = try_insert(&s, &(struct val){.id = i, .val = i}.elem);
         CHECK(occupied(&e), false);
+        CHECK(validate(&s), true);
         e = try_insert(&s, &(struct val){.id = i, .val = i}.elem);
         CHECK(occupied(&e), true);
+        CHECK(validate(&s), true);
         struct val const *const v = unwrap(&e);
         CHECK(v == NULL, false);
         CHECK(v->id, i);
@@ -118,11 +120,13 @@ CHECK_BEGIN_STATIC_FN(fomap_test_insert_and_find)
     {
         CHECK(contains(&s, &i), true);
         CHECK(occupied(entry_r(&s, &i)), true);
+        CHECK(validate(&s), true);
     }
     for (int i = 1; i < size; i += 2)
     {
         CHECK(contains(&s, &i), false);
         CHECK(occupied(entry_r(&s, &i)), false);
+        CHECK(validate(&s), true);
     }
     CHECK_END_FN();
 }
