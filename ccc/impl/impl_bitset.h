3243d9c409bc6eda6ec526c5a42c78b169faaf85
Alexander Lopez <agl.alexglopez@gmail.com>
2024-12-18 17:13:59 -0800

    simplify macro calculations

--- ccc/impl/impl_bitset.h
+++ ccc/impl/impl_bitset.h
@@ -19,12 +19,11 @@ struct ccc_bitset_
 };
 
 /** @private Number of bits in a block. */
-#define CCC_IMPL_BTST_BLOCK_BITS ((size_t)(sizeof(ccc_bitblock_) * CHAR_BIT))
+#define CCC_IMPL_BTST_BLOCK_BITS (sizeof(ccc_bitblock_) * CHAR_BIT)
 
 /** @private */
 #define ccc_impl_bitblocks(bit_cap)                                            \
-    ((size_t)(((bit_cap) + (CCC_IMPL_BTST_BLOCK_BITS - 1))                     \
-              / CCC_IMPL_BTST_BLOCK_BITS))
+    (((bit_cap) + (CCC_IMPL_BTST_BLOCK_BITS - 1)) / CCC_IMPL_BTST_BLOCK_BITS)
 
 /** @private */
 #define ccc_impl_btst_init(bitblock_ptr, cap, alloc_fn, aux)                   \
