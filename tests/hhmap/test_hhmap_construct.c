58654f73f8274cd3583db55ff264440fddf118ae
Alexander Lopez <agl.alexglopez@gmail.com>
2025-01-17 15:53:48 -0800

    passing initial bout of tests

--- tests/hhmap/test_hhmap_construct.c
+++ tests/hhmap/test_hhmap_construct.c
@@ -58,7 +58,7 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_static_init)
 
     /* Inserting default value before an in place modification is possible. */
     ccc_handle h = hhm_or_insert(hhm_entry_r(&static_fh, &def.key), &def.e);
-    CHECK(h, true);
+    CHECK(h != 0, true);
     struct val *v = hhm_at(&static_fh, h);
     CHECK(v != NULL, true);
     v->val++;
@@ -70,17 +70,19 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_static_init)
 
     /* Modifying an existing value or inserting default is possible when no
        auxiliary input is needed. */
-    struct val *v2 = hhm_or_insert(
-        hhm_and_modify(hhm_entry_r(&static_fh, &def.key), mod), &def.e);
+    h = hhm_or_insert(hhm_and_modify(hhm_entry_r(&static_fh, &def.key), mod),
+                      &def.e);
+    struct val *const v2 = hhm_at(&static_fh, h);
     CHECK((v2 != NULL), true);
     CHECK(inserted->key, 137);
     CHECK(v2->val, 6);
 
     /* Modifying an existing value that requires external input is also
        possible with slightly different signature. */
-    struct val *v3 = hhm_or_insert(
+    h = hhm_or_insert(
         hhm_and_modify_aux(hhm_entry_r(&static_fh, &def.key), modw, &def.key),
         &def.e);
+    struct val *const v3 = hhm_at(&static_fh, h);
     CHECK((v3 != NULL), true);
     CHECK(inserted->key, 137);
     CHECK(v3->val, 137);
@@ -94,8 +96,14 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_copy_no_alloc)
     handle_hash_map dst = hhm_init((struct val[13]){}, 13, e, key, NULL,
                                    hhmap_int_zero, hhmap_id_eq, NULL);
     (void)hhm_insert(&src, &(struct val){.key = 0}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
     (void)hhm_insert(&src, &(struct val){.key = 1, .val = 1}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
+    CHECK(hhm_contains(&src, &(int){1}), true);
     (void)hhm_insert(&src, &(struct val){.key = 2, .val = 2}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
+    CHECK(hhm_contains(&src, &(int){1}), true);
+    CHECK(hhm_contains(&src, &(int){2}), true);
     CHECK(hhm_size(&src), 3);
     CHECK(hhm_is_empty(&dst), true);
     ccc_result res = hhm_copy(&dst, &src, NULL);
@@ -135,8 +143,14 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_copy_alloc)
     handle_hash_map dst = hhm_init((struct val *)NULL, 0, e, key, std_alloc,
                                    hhmap_int_zero, hhmap_id_eq, NULL);
     (void)hhm_insert(&src, &(struct val){.key = 0}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
     (void)hhm_insert(&src, &(struct val){.key = 1, .val = 1}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
+    CHECK(hhm_contains(&src, &(int){1}), true);
     (void)hhm_insert(&src, &(struct val){.key = 2, .val = 2}.e);
+    CHECK(hhm_contains(&src, &(int){0}), true);
+    CHECK(hhm_contains(&src, &(int){1}), true);
+    CHECK(hhm_contains(&src, &(int){2}), true);
     CHECK(hhm_size(&src), 3);
     CHECK(hhm_is_empty(&dst), true);
     ccc_result res = hhm_copy(&dst, &src, std_alloc);
@@ -188,14 +202,17 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_functional)
     CHECK(hhm_is_empty(&fh), true);
     struct val def = {.key = 137, .val = 0};
     hhmap_entry ent = hhm_entry(&fh, &def.key);
-    CHECK(hhm_unwrap(&ent) == NULL, true);
-    struct val *v = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    CHECK(hhm_unwrap(&ent), false);
+    ccc_handle h = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    struct val *v = hhm_at(&fh, h);
     CHECK(v != NULL, true);
     v->val += 1;
-    struct val const *const inserted = hhm_get_key_val(&fh, &def.key);
+    h = hhm_get_key_val(&fh, &def.key);
+    struct val const *const inserted = hhm_at(&fh, h);
     CHECK((inserted != NULL), true);
     CHECK(inserted->val, 1);
-    v = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    h = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    v = hhm_at(&fh, h);
     CHECK(v != NULL, true);
     v->val += 1;
     CHECK(inserted->val, 2);
@@ -207,18 +224,20 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_macros)
     handle_hash_map fh = hhm_init((struct val[5]){}, 5, e, key, NULL,
                                   hhmap_int_zero, hhmap_id_eq, NULL);
     CHECK(hhm_is_empty(&fh), true);
-    CHECK(get_key_val(&fh, &(int){137}) == NULL, true);
+    CHECK(hhm_get_key_val(&fh, &(int){137}), false);
     int const key = 137;
     int mut = 99;
     /* The function with a side effect should execute. */
-    struct val *inserted = hhm_or_insert_w(
-        hhm_entry_r(&fh, &key), (struct val){.key = key, .val = def(&mut)});
+    ccc_handle h = hhm_or_insert_w(hhm_entry_r(&fh, &key),
+                                   (struct val){.key = key, .val = def(&mut)});
+    struct val *inserted = hhm_at(&fh, h);
     CHECK(inserted != NULL, true);
     CHECK(mut, 100);
     CHECK(inserted->val, 0);
     /* The function with a side effect should NOT execute. */
-    struct val *v = hhm_or_insert_w(hhm_entry_r(&fh, &key),
-                                    (struct val){.key = key, .val = def(&mut)});
+    h = hhm_or_insert_w(hhm_entry_r(&fh, &key),
+                        (struct val){.key = key, .val = def(&mut)});
+    struct val *v = hhm_at(&fh, h);
     CHECK(v != NULL, true);
     v->val++;
     CHECK(mut, 100);
@@ -237,29 +256,32 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_hhm_and_modify_functional)
      */
     hhmap_entry *ent = hhm_and_modify(hhm_entry_r(&fh, &def.key), mod);
     CHECK(hhm_occupied(ent), false);
-    CHECK((hhm_unwrap(ent) == NULL), true);
+    CHECK(hhm_unwrap(ent), false);
 
     /* Inserting default value before an in place modification is possible. */
-    struct val *v = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    ccc_handle h = hhm_or_insert(hhm_entry_r(&fh, &def.key), &def.e);
+    struct val *v = hhm_at(&fh, h);
     CHECK(v != NULL, true);
     v->val++;
-    struct val const *const inserted = hhm_get_key_val(&fh, &def.key);
+    h = hhm_get_key_val(&fh, &def.key);
+    struct val const *const inserted = hhm_at(&fh, h);
     CHECK((inserted != NULL), true);
     CHECK(inserted->key, 137);
     CHECK(inserted->val, 1);
 
     /* Modifying an existing value or inserting default is possible when no
        auxiliary input is needed. */
-    struct val *v2 = hhm_or_insert(
-        hhm_and_modify(hhm_entry_r(&fh, &def.key), mod), &def.e);
+    h = hhm_or_insert(hhm_and_modify(hhm_entry_r(&fh, &def.key), mod), &def.e);
+    struct val *v2 = hhm_at(&fh, h);
     CHECK((v2 != NULL), true);
     CHECK(inserted->key, 137);
     CHECK(v2->val, 6);
 
     /* Modifying an existing value that requires external input is also
        possible with slightly different signature. */
-    struct val *v3 = hhm_or_insert(
+    h = hhm_or_insert(
         hhm_and_modify_aux(hhm_entry_r(&fh, &def.key), modw, &def.key), &def.e);
+    struct val *v3 = hhm_at(&fh, h);
     CHECK((v3 != NULL), true);
     CHECK(inserted->key, 137);
     CHECK(v3->val, 137);
@@ -276,17 +298,16 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_hhm_and_modify_macros)
      */
     hhmap_entry *ent = hhm_and_modify(hhm_entry_r(&fh, &(int){137}), mod);
     CHECK(hhm_occupied(ent), false);
-    CHECK((hhm_unwrap(ent) == NULL), true);
+    CHECK(hhm_unwrap(ent), false);
 
     int mut = 99;
 
     /* Inserting default value before an in place modification is possible. */
-    struct val *v = hhm_or_insert_w(
-        hhm_and_modify_w(hhm_entry_r(&fh, &(int){137}), struct val,
-                         {
-                             T->val = gen(&mut);
-                         }),
-        (struct val){.key = 137, .val = def(&mut)});
+    ccc_handle h
+        = hhm_or_insert_w(hhm_and_modify_w(hhm_entry_r(&fh, &(int){137}),
+                                           struct val, { T->val = gen(&mut); }),
+                          (struct val){.key = 137, .val = def(&mut)});
+    struct val *v = hhm_at(&fh, h);
     CHECK((v != NULL), true);
     CHECK(v->key, 137);
     CHECK(v->val, 0);
@@ -294,9 +315,9 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_hhm_and_modify_macros)
 
     /* Modifying an existing value or inserting default is possible when no
        auxiliary input is needed. */
-    struct val *v2
-        = hhm_or_insert_w(hhm_and_modify(hhm_entry_r(&fh, &(int){137}), mod),
-                          (struct val){.key = 137, .val = def(&mut)});
+    h = hhm_or_insert_w(hhm_and_modify(hhm_entry_r(&fh, &(int){137}), mod),
+                        (struct val){.key = 137, .val = def(&mut)});
+    struct val *v2 = hhm_at(&fh, h);
     CHECK((v2 != NULL), true);
     CHECK(v2->key, 137);
     CHECK(v2->val, 5);
@@ -307,12 +328,10 @@ CHECK_BEGIN_STATIC_FN(hhmap_test_hhm_entry_hhm_and_modify_macros)
        lazy evaluation. The function gen executes with its side effect,
        but the function def does not execute and therefore does not modify
        mut. */
-    struct val *v3 = hhm_or_insert_w(
-        hhm_and_modify_w(hhm_entry_r(&fh, &(int){137}), struct val,
-                         {
-                             T->val = gen(&mut);
-                         }),
-        (struct val){.key = 137, .val = def(&mut)});
+    h = hhm_or_insert_w(hhm_and_modify_w(hhm_entry_r(&fh, &(int){137}),
+                                         struct val, { T->val = gen(&mut); }),
+                        (struct val){.key = 137, .val = def(&mut)});
+    struct val *v3 = hhm_at(&fh, h);
     CHECK((v3 != NULL), true);
     CHECK(v3->key, 137);
     CHECK(v3->val, 42);
