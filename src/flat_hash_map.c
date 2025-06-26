149ec3b8bc5bf5feb3cbfc64109a2d00c6c01bc7
Alexander Lopez <agl.alexglopez@gmail.com>
2025-06-26 00:04:37 -0700

    revert and only reintroduce change that catches plus

--- src/flat_hash_map.c
+++ src/flat_hash_map.c
@@ -307,7 +307,6 @@ static ccc_tribool eq_fn(struct ccc_fhmap const *h, void const *key, size_t i);
 static uint64_t hash_fn(struct ccc_fhmap const *h, void const *any_key);
 static void *key_at(struct ccc_fhmap const *h, size_t i);
 static void *data_at(struct ccc_fhmap const *h, size_t i);
-static ccc_fhm_tag *tag_pos(void const *data, size_t sizeof_type, size_t mask);
 static void *key_in_slot(struct ccc_fhmap const *h, void const *slot);
 static void *swap_slot(struct ccc_fhmap const *h);
 static ccc_ucount data_i(struct ccc_fhmap const *h, void const *data_slot);
@@ -1382,7 +1381,9 @@ rehash_resize(struct ccc_fhmap *const h, size_t const to_add,
     new_h.remain = mask_to_load_factor_cap(new_h.mask);
     new_h.data = new_buf;
     /* Our static assertions at start of file guarantee this is correct. */
-    new_h.tag = tag_pos(new_buf, new_h.sizeof_type, new_h.mask);
+    new_h.tag
+        = (ccc_fhm_tag *)((char *)new_buf
+                          + mask_to_data_bytes(new_h.sizeof_type, new_h.mask));
     (void)memset(new_h.tag, TAG_EMPTY, mask_to_tag_bytes(new_h.mask));
     for (size_t i = 0; i < (h->mask + 1); ++i)
     {
@@ -1422,7 +1423,8 @@ check_init(struct ccc_fhmap *const h, size_t const required_total_cap)
         {
             return CCC_RESULT_ARG_ERROR;
         }
-        h->tag = tag_pos(h, h->sizeof_type, h->mask);
+        h->tag = (ccc_fhm_tag *)((char *)h->data
+                                 + mask_to_data_bytes(h->sizeof_type, h->mask));
         (void)memset(h->tag, TAG_EMPTY, mask_to_tag_bytes(h->mask));
     }
     return CCC_RESULT_OK;
@@ -1459,13 +1461,6 @@ data_at(struct ccc_fhmap const *const h, size_t const i)
 {
     assert(i <= h->mask);
     return h->tag - ((i + 1) * h->sizeof_type);
-}
-
-static inline ccc_fhm_tag *
-tag_pos(void const *const data, size_t const sizeof_type, size_t const mask)
-{
-    return (ccc_fhm_tag *)((char *)data
-                           + mask_to_data_bytes(sizeof_type, mask));
 }
 
 static inline ccc_ucount
