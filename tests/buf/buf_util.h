ad620cb5f38bd3eb1112d6903624389cc2ea6eff
Alexander Lopez <agl.alexglopez@gmail.com>
2025-07-11 23:24:41 -0700

    improve sort comment

--- tests/buf/buf_util.h
+++ tests/buf/buf_util.h
@@ -7,7 +7,9 @@
 /** Sorts a buffer according to its size assuming that elements are stored from
 indices [0, N) where N is the size not capacity of the buffer. Requires a
 comparison function from the user and one swap slot equivalent to the size of an
-element stored in the buffer. */
+element stored in the buffer. Elements are sorted in a non-decreasing order.
+Therefore if a non-increasing list is needed simply reverse the return of
+the comparison function for non-equivalent values. */
 ccc_result sort(ccc_buffer *b, ccc_any_type_cmp_fn *fn, void *swap);
 
 #endif /* BUF_UTIL_H */
