7a6bb6e16688aaa11fe8cc16b5c95e93a772456f
Alexander Lopez <agl.alexglopez@gmail.com>
2025-11-19 13:54:08 -0800

    add constructor within function scope for maps

--- tests/adaptive_map/test_adaptive_map_construct.c
+++ tests/adaptive_map/test_adaptive_map_construct.c
@@ -8,6 +8,14 @@
 #include "checkers.h"
 #include "traits.h"
 
+static CCC_Adaptive_map
+construct_empty(void)
+{
+    CCC_Adaptive_map map = CCC_adaptive_map_initialize(struct Val, elem, key,
+                                                       id_order, NULL, NULL);
+    return map;
+}
+
 check_static_begin(adaptive_map_test_empty)
 {
     CCC_Adaptive_map s = CCC_adaptive_map_initialize(struct Val, elem, key,
@@ -16,8 +24,26 @@ check_static_begin(adaptive_map_test_empty)
     check_end();
 }
 
+/** If the user constructs a node style map from a helper function, the map
+cannot have any self referential fields, such as nil or sentinel nodes. If
+the map is initialized on the stack those self referential fields will become
+invalidated after the constructing function ends. This leads to a dangling
+reference to stack memory that no longer exists. Disastrous. The solution is
+to never implement sentinels that refer to a memory address on the map struct
+itself. */
+check_static_begin(adaptive_map_test_construct)
+{
+    struct Val push = {};
+    CCC_Adaptive_map map = construct_empty();
+    CCC_Entry entry = CCC_adaptive_map_insert_or_assign(&map, &push.elem);
+    check(CCC_adaptive_map_validate(&map), true);
+    check(CCC_entry_insert_error(&entry), false);
+    check(CCC_entry_occupied(&entry), false);
+    check(CCC_adaptive_map_count(&map).count, 1);
+    check_end();
+}
 int
 main()
 {
-    return check_run(adaptive_map_test_empty());
+    return check_run(adaptive_map_test_empty(), adaptive_map_test_construct());
 }
