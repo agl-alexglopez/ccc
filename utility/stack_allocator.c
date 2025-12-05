0f4a56e6283b3a9f628542bc6a539fb08c1b0720
Alexander Lopez <agl.alexglopez@gmail.com>
2025-12-05 10:44:31 -0800

    roundup stack allocator requests

--- utility/stack_allocator.c
+++ utility/stack_allocator.c
@@ -1,17 +1,20 @@
-#include "stack_allocator.h"
+#include <stddef.h>
+
 #include "ccc/types.h"
+#include "stack_allocator.h"
 
 void *
-stack_allocator_allocate(CCC_Allocator_context const context)
+stack_allocator_allocate(CCC_Allocator_context context)
 {
     if (!context.bytes || context.input || !context.context)
     {
         return NULL;
     }
     struct Stack_allocator *const allocator = context.context;
-    if (context.bytes % allocator->sizeof_type != 0)
+    size_t const remainder = context.bytes % allocator->sizeof_type;
+    if (remainder)
     {
-        return NULL;
+        context.bytes = context.bytes + allocator->sizeof_type - remainder;
     }
     if (allocator->bytes_occupied + context.bytes > allocator->bytes_capacity)
     {
